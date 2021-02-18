34565f89510ea3651a75812b9998da8ed0e2e6ae
Samip <samipgnyawali15@gmail.com>
2021-02-18 14:03:43 +0545

    Added google map

--- app/src/main/java/com/sam/googlemapexample/MainActivity.kt
+++ app/src/main/java/com/sam/googlemapexample/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.sam.googlemapexample
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync {
+            googleMap = it
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return@getMapAsync
+            }
+            googleMap.isMyLocationEnabled = true
+            val location = LatLng(13.03,77.60)
+            googleMap.addMarker(MarkerOptions().position(location).title("My Location"))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location,14f))
+            val location1 = LatLng(13.00,78.11)
+            googleMap.addMarker(MarkerOptions().position(location1).title("Bangalore"))
+        }
     }
 }
